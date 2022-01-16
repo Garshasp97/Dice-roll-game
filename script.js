6f51ad6ee5c0e29e596420b832a9998e8c1fd4d6
Garshasp97 <Zeljkovickristian8@gmail.com>
2022-01-16 20:53:39 +0100

    added game reset delay when a player wins

--- script.js
+++ script.js
@@ -22,13 +22,31 @@ const activePlayer1 = document.querySelector('.player--1');
 
 // Starting conditions
 
-const scores = [0, 0];
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 let currentScore = 0;
 let currentPlayer = 0;
 let holdScore = 0;
+
+// Reset game function
+function resetGame() {
+  currentPlayer = 0;
+  activePlayer0.classList.add('player--active');
+  activePlayer1.classList.remove('player--active');
+  currentScore = 0;
+  document.getElementById(`current--${0}`).textContent = 0;
+  document.getElementById('name--0').textContent = 'PLAYER 1';
+  activePlayer0.classList.remove('player--winner');
+  document.getElementById(`current--${1}`).textContent = 0;
+  document.getElementById('name--1').textContent = 'PLAYER 2';
+  activePlayer1.classList.remove('player--winner');
+  holdScore = 0;
+  holdScorePlayer0 = 0;
+  score0El.textContent = 0;
+  holdScorePlayer1 = 0;
+  score1El.textContent = 0;
+}
 
 // Rolling the dice
 rollDiceBtn.addEventListener('click', function () {
@@ -83,34 +101,34 @@ holdScoreBtn.addEventListener('click', function () {
     holdScorePlayer0 += holdScore;
     holdScore = 0;
     score0El.textContent = holdScorePlayer0;
-    activePlayer0.classList.remove('player--active');
-    document.getElementById(`current--${currentPlayer}`).textContent = 0;
-    currentPlayer = 1;
-    activePlayer1.classList.add('player--active');
-    currentScore = 0;
+    if (holdScorePlayer0 >= 100) {
+      activePlayer0.classList.add('player--winner');
+      document.getElementById('name--0').textContent = '🎉YOU WIN🎉';
+      setTimeout(resetGame, 6000);
+    } else {
+      activePlayer0.classList.remove('player--active');
+      document.getElementById(`current--${currentPlayer}`).textContent = 0;
+      currentPlayer = 1;
+      activePlayer1.classList.add('player--active');
+      currentScore = 0;
+    }
   } else {
     holdScore = currentScore;
     holdScorePlayer1 += holdScore;
     holdScore = 0;
     score1El.textContent = holdScorePlayer1;
-    activePlayer1.classList.remove('player--active');
-    document.getElementById(`current--${currentPlayer}`).textContent = 0;
-    currentPlayer = 0;
-    activePlayer0.classList.add('player--active');
-    currentScore = 0;
+    if (holdScorePlayer1 >= 100) {
+      activePlayer1.classList.add('player--winner');
+      document.getElementById('name--1').textContent = '🎉YOU WIN🎉';
+      setTimeout(resetGame, 6000);
+    } else {
+      activePlayer1.classList.remove('player--active');
+      document.getElementById(`current--${currentPlayer}`).textContent = 0;
+      currentPlayer = 0;
+      activePlayer0.classList.add('player--active');
+      currentScore = 0;
+    }
   }
 });
 
-newGameBtn.addEventListener('click', function () {
-  currentPlayer = 0;
-  activePlayer0.classList.add('player--active');
-  activePlayer1.classList.remove('player--active');
-  currentScore = 0;
-  document.getElementById(`current--${0}`).textContent = 0;
-  document.getElementById(`current--${1}`).textContent = 0;
-  holdScore = 0;
-  holdScorePlayer0 = 0;
-  score0El.textContent = 0;
-  holdScorePlayer1 = 0;
-  score1El.textContent = 0;
-});
+newGameBtn.addEventListener('click', resetGame);
